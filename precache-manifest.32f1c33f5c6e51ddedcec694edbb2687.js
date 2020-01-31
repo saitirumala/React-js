938a425756f05aa934ce67fdd5e1f06038398a74
saitirumala <saiprasadtirumala.t@gmail.com>
2020-01-31 13:01:55 +0530

    first commit

---
+++ precache-manifest.32f1c33f5c6e51ddedcec694edbb2687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f02b0e2baaeaacf89b97254293991f6",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "f83d389bc2fb39504b3f",
+    "url": "/React/static/css/2.b4d5ff61.chunk.css"
+  },
+  {
+    "revision": "fde79a4e947cd7b73d36",
+    "url": "/React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f83d389bc2fb39504b3f",
+    "url": "/React/static/js/2.d5bfa09b.chunk.js"
+  },
+  {
+    "revision": "fde79a4e947cd7b73d36",
+    "url": "/React/static/js/main.f0450e18.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  }
+]);
